24e14978cdfd11c396bce279fc70cf90726a518d
kelvinrandu <randukelvin@gmail.com>
2021-07-07 17:29:28 +0300

    refractored toggle function

--- js/app.js
+++ js/app.js
@@ -1,11 +1,6 @@
 const navToggle = document.querySelector('.nav-toggle');
 const links = document.querySelector('.links');
-console.log(navToggle)
  navToggle.addEventListener('click',function(){
     
-     if(links.classList.contains('show-links')){
-         links.classList.remove('show-links');
-     }else{
-         links.classList.add('show-links');
-     }
+links.classList.toggle("show-links")
  })
